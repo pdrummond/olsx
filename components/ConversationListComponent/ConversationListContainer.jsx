14adb3c94e1f1a6d5228475b386f5dec89c1dd0e
Paul Drummond <paul.drummond@iode.co.uk>
2016-02-08 16:42:08 +0000

    Projects now have proper validation, schema and method security.

--- components/ConversationListComponent/ConversationListContainer.jsx
+++ components/ConversationListComponent/ConversationListContainer.jsx
@@ -36,7 +36,7 @@ ConversationListContainer = React.createClass({
         Conversations.methods.addConversation.call({subject}, (err) => {
             if(err) {
                 toastr.error('Oops! Something went wrong creating conversation - please try again.');
-                console.error('Error creating conversation: ' + err.reason);
+                console.error('Error creating conversation: ' + err);
             }
         });
         ReactDOM.findDOMNode(this.refs.textInput).value = "";

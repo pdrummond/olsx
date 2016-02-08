9a12b0a36b56aaa2a8806ba536466133174a630f
Paul Drummond <paul.drummond@iode.co.uk>
2016-02-08 12:19:16 +0000

    Added proper schema, validation and method security for tasks

--- components/MessageListComponent/Message.jsx
+++ components/MessageListComponent/Message.jsx
@@ -4,10 +4,21 @@ Message = React.createClass({
     },
 
     render() {
-       return (
-           <li className={this.props.message.isError ? 'message-item error': this.props.message.isSuccess?'message-item success':'message-item'}><b>{this.props.message.createdByName}:</b> <span className="message-content">{this.props.message.content}</span>
-               <div className="message-created-at">{this.props.message.seq} </div>
-           </li>
-       )
+           if(this.props.message.isError || this.props.message.isSuccess) {
+               return (
+               <li className={this.props.message.isError ? 'message-item error': this.props.message.isSuccess?'message-item success':'message-item'}>
+                   <span className="message-content">{this.props.message.content}</span>
+                   <div className="message-created-at">{this.props.message.seq} </div>
+               </li>
+               );
+           } else {
+               return (
+               <li className={this.props.message.isError ? 'message-item error': this.props.message.isSuccess?'message-item success':'message-item'}>
+                   <b>{this.props.message.createdByName}:</b> <span
+                   className="message-content">{this.props.message.content}</span>
+                   <div className="message-created-at">{this.props.message.seq} </div>
+               </li>
+               );
+           }
     }
 });
--- server/task-methods.js
+++ server/task-methods.js
@@ -1,29 +1,46 @@
-Meteor.methods({
-    addTask: function(title, conversationId, messageId) {
+Tasks.methods = {};
 
+Tasks.methods.addTask = new ValidatedMethod({
+    name: 'Tasks.methods.addTask',
+
+    validate: new SimpleSchema({
+        description: {type: String},
+        conversationId: {type: String},
+        messageId: {type: String}
+    }).validator(),
+
+    run({description, conversationId, messageId}) {
         var conversation = Conversations.findOne(conversationId);
 
-        if(conversation == null) {
-            throw Meteor.Error('Error adding task', 'invalid conversation ID: ' + conversationId);
+        if (conversation == null) {
+            throw Meteor.Error(
+                'Tasks.methods.addTask.invalid-conversation-id',
+                'Error adding task - invalid conversation ID: ' + conversationId
+            );
         }
 
         var project = Projects.findOne(conversation.defaultProjectId);
 
-        if(project == null) {
-            throw Meteor.Error('Unable to find user default project for user ' + Meteor.user().username)
+        if (project == null) {
+            throw Meteor.Error(
+                'Tasks.methods.addTask.invalid-default-project',
+                'Unable to find user default project for conversation ' + conversationId
+            )
         }
 
         var seq = incrementCounter('counters', project._id);
         var key = '';
-        if(project.key) {
+        if (project.key) {
             key = project.key + '-' + seq;
         } else {
             key = seq;
         }
 
         var task = {
-            title: title,
+            description:description,
             status: 'new',
+            createdAt: new Date(),
+            updatedAt: new Date(),
             createdBy: Meteor.userId(),
             createdByName: Meteor.user().username,
             updatedBy: Meteor.userId(),

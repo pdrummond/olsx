84aa76770855688f88b2a33cc6b49db74de4cb93
Paul Drummond <paul.drummond@iode.co.uk>
2016-02-11 18:39:24 +0000

    Layout and minor style improvements.

--- components/MessageListComponent/SystemMessage.jsx
+++ components/MessageListComponent/SystemMessage.jsx
@@ -2,7 +2,10 @@ SystemMessage = React.createClass({
     render() {
         return (
             <li className={this.props.message.isError?'message-item error':'message-item success'}>
-                <span className="message-content"><i className="fa fa-exchange"></i> {this.props.message.content}</span>
+                <span className="message-content">
+                    <i className="fa fa-dot-circle-o"></i> {this.props.message.content}
+                </span>
+                <span className="message-created-at"> {moment(this.props.message.createdAt).fromNow()} </span>
             </li>
         );
     }

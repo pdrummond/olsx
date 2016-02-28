d2d5681db6f00811cff9c7b21f0921ea3de60253
Paul Drummond <paul.drummond@iode.co.uk>
2016-02-28 16:02:52 +0000

    OLS-123: Re-designed activity messages for releases

--- packages/lib/activity-type-api.js
+++ packages/lib/activity-type-api.js
@@ -1,5 +1,6 @@
 Ols.ActivityType = {
     ACTIVITY_TYPE_ITEM: 'item',
     ACTIVITY_TYPE_MEMBER: 'member',
-    ACTIVITY_TYPE_PROJECT: 'project'
+    ACTIVITY_TYPE_PROJECT: 'project',
+    ACTIVITY_TYPE_RELEASE: 'release'
 };

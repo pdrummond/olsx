3b96cd2f2612fc6329238defabaa41ec628dcd4e
Paul Drummond <paul.drummond@iode.co.uk>
2016-03-03 19:55:08 +0000

    #OLS-172: Backlog should only show open TASKS and BUGS

--- components/BacklogComponent/BacklogComponent.jsx
+++ components/BacklogComponent/BacklogComponent.jsx
@@ -4,7 +4,12 @@ BacklogComponent = React.createClass({
             <ItemListComponent
                 projectId={this.props.projectId}
                 projectTemplate={this.props.projectTemplate}
-                filter={ {milestoneId: {$exists:false}, isArchived:false} } />
+                filter={{
+                    milestoneId: {$exists:false},
+                    isArchived:false,
+                    status: {$lte: Ols.Status.OPEN},
+                    $or: [{subType: Ols.Item.ACTION_SUBTYPE_TASK}, {subType: Ols.Item.ISSUE_SUBTYPE_BUG}]
+                }} />
         )
     }
 });
